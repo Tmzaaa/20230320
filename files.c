db1c58e2d3e1674d7b8b1db7d254b25bacf4c62b
Tmzaaa <2723478933@qq.com>
2023-12-01 19:09:13 +0800

    首次代码提交

---
+++ files.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("xxxxxxxxxxxxx");
+	printf("xxxxxxxxxxxxx");
+	printf("xxxxxxxxxxxxx");
+	printf("xxxxxxxxxxxxx");
+	printf("xxxxxxxxxxxxx");
+	printf("xxxxxxxxxxxxx");
+	printf("xxxxxxxxxxxxx");
+	printf("xxxxxxxxxxxxx");
+}
